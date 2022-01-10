d3d0483432e798d0bb2e3ea665710e4382434146
pippopil <voood234@gmail.com>
2022-01-10 14:00:53 +0300

    Complete

---
+++ src/main/java/ru/netology/domain/GeoInfo.java
@@ -0,0 +1,6 @@
+package main.java.ru.netology.domain;
+
+public class GeoInfo {
+    String type;
+    String coordinates;
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -3,12 +3,26 @@ package main.java.ru.netology.domain;
 public class Post {
     private int id;
     private int date;
+    private int owner_Id;
+    private int created_by;
+    private int from_id;
     private String text;
-    private int replyOwnerId;
-    private int replyPostId;
+    private int reply_Owner_Id;
+    private int reply_Post_Id;
+    private int post_Type;
+    private  int signer_Id;
+    private boolean is_Favorite;
+    private int can_Pin;
+    private int can_Delete;
+    private int can_edit;
+    private int is_Pinned;
+    private int marked_Ad_Ads;
+    private int postponed_Id;
     private LikesInfo likesInfo;
     private CommentsInfo commentsInfo;
     private RepostsInfo repostInfo;
     private ViewsInfo viewsInfo;
+    private GeoInfo geosInfo;
+
 }
 //getter+setter;
